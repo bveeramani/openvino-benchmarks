0bf4c05595b95fabae66fc1db1bab5bff39949ee
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-22 18:31:34 -0500

    Update compile_models.sh

--- scripts/sh/compile_models.sh
+++ scripts/sh/compile_models.sh
@@ -63,19 +63,26 @@ caffe R-CNN http://dl.caffe.berkeleyvision.org/bvlc_reference_rcnn_ilsvrc13.caff
 onnx MobileNetV2 https://s3.amazonaws.com/onnx-model-zoo/mobilenet/mobilenetv2-1.0/mobilenetv2-1.0.onnx
 onnx ResNet-18V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet18v2/resnet18v2.onnx
 onnx ResNet-34V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet34v2/resnet34v2.onnx
-onnx ResNet-52V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet34v2/resnet52v2.onnx
+onnx ResNet-52V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet52v2/resnet52v2.onnx
 onnx ResNet-101V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet101v2/resnet101v2.onnx
 onnx ResNet-152V2 https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet152v2/resnet152v2.onnx
 onnx SqueezeNet https://s3.amazonaws.com/onnx-model-zoo/squeezenet/squeezenet1.1/squeezenet1.1.onnx
 
 # DenseNet is a special case
-curl -o densenet121.tar.gz https://s3.amazonaws.com/download.onnx/models/opset_8/densenet121.tar.gz
-tar -xzf densenet121.tar.gz
-mv densenet121/model.onnx $TEMP_DIR/DenseNet-121.onnx
-python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo.py \
-    --input_model $TEMP_DIR/DenseNet-121.onnx \
-    --output_dir $PROJECT_DIR/models/fp32 \
-    --model_name DenseNet-121 \
-    --data_type FP32
-rm densenet121.tar.gz
-rm -rf densenet121
+if [ ! -f "$TEMP_DIR/DenseNet-121.onnx" ];
+then
+    curl -o densenet121.tar.gz https://s3.amazonaws.com/download.onnx/models/opset_8/densenet121.tar.gz
+    tar -xzf densenet121.tar.gz
+    mv densenet121/model.onnx $TEMP_DIR/DenseNet-121.onnx
+    rm densenet121.tar.gz
+    rm -rf densenet121
+fi
+
+if [ ! -f "$PROJECT_DIR/models/fp32/DenseNet-121.xml" ];
+then
+    python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo.py \
+        --input_model $TEMP_DIR/DenseNet-121.onnx \
+        --output_dir $PROJECT_DIR/models/fp32 \
+        --model_name DenseNet-121 \
+        --data_type FP32
+fi
