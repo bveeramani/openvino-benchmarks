29b8cdf2d8f6fdb459a011b831d6c8640d8dacdb
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-22 13:16:09 -0500

    Delete profile_models.bat

--- scripts/bat/profile_models.bat
+++
@@ -1,8 +0,0 @@
-SET root=%~dp0..\..
-
-FOR /R %root%\models\%precision% %%M IN (*.xml) DO (
-    FOR %%A IN (sync, async) DO (
-      python %root%\scripts\python\benchmark.py --model %%M --device %1^
-      --batch_size 32 -f .\benchmarks.csv --num_infer_requests 2 --api %%A
-    )
-)
