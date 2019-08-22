5b4fc26655e621bdf60aff2f609f228b57719f76
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-22 17:41:32 -0500

    Update compile_models.sh

--- scripts/sh/compile_models.sh
+++ scripts/sh/compile_models.sh
@@ -1,13 +1,13 @@
 TEMP_DIR=/tmp/models/
 INSTALL_DIR=/opt/intel/openvino
-MODEL_DIR=~/models
+PROJECT_DIR=$(dirname "$0")/../..
 
 curl -o $TEMP_DIR/AlexNet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
 curl -o $TEMP_DIR/AlexNet.prototxt https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt
 python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo.py \
     --input_model $TEMP_DIR/AlexNet.caffemodel \
     --input_proto $TEMP_DIR/AlexNet.prototxt \
-    --output_dir $MODEL_DIR \
+    --output_dir $PROJECT_DIR/models/fp32 \
     --model_name AlexNet \
     --data_type FP32
 
