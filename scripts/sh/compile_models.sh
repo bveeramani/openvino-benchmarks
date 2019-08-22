a13cb4cd9449e77c3ad25aea808e06560ea6b4cd
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-22 17:52:31 -0500

    Update compile_models.sh

--- scripts/sh/compile_models.sh
+++ scripts/sh/compile_models.sh
@@ -1,17 +1,27 @@
-TEMP_DIR=/tmp/models/
+#!/bin/bash
+TEMP_DIR=/tmp/models
 INSTALL_DIR=/opt/intel/openvino
 PROJECT_DIR=$(dirname "$0")/../..
 
 mkdir -p $TEMP_DIR
 
-curl -o $TEMP_DIR/AlexNet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
-curl -o $TEMP_DIR/AlexNet.prototxt https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt
-python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo.py \
-    --input_model $TEMP_DIR/AlexNet.caffemodel \
-    --input_proto $TEMP_DIR/AlexNet.prototxt \
-    --output_dir $PROJECT_DIR/models/fp32 \
-    --model_name AlexNet \
-    --data_type FP32
+caffe()
+{
+    MODEL_NAME=$1
+    CAFFEMODEL_URL=$2
+    PROTOTXT_URL=$3
 
-# curl -o GoogleNet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
-# curl -o GoogleNet.prototxt https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/deploy.prototxt
+    curl -o $TEMP_DIR/$MODEL_NAME.caffemodel $CAFFEMODEL_URL
+    curl -o $TEMP_DIR/$MODEL_NAME.prototxt $PROTOTXT_URL
+    python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo.py \
+        --input_model $TEMP_DIR/$MODEL_NAME.caffemodel \
+        --input_proto $TEMP_DIR/$MODEL_NAME.prototxt \
+        --output_dir $PROJECT_DIR/models/fp32 \
+        --model_name $MODEL_NAME \
+        --data_type FP32
+}
+
+
+caffe AlexNet \
+    http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel \
+    https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt
